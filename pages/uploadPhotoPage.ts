ba3be8010956bc08a1d25ac67752e63ba77908c1
Tiago Marques <Tiago Marques>
2026-07-19 00:45:53 -0300

    Adicionando os testes da funcionallidade de upload de imagens

---
+++ pages/uploadPhotoPage.ts
@@ -0,0 +1,41 @@
+import { Page, expect } from '@playwright/test'
+
+export class UploadPhotoPage {
+    readonly page: Page;
+    static readonly BASE_URL = 'file:///C:/Projects/QaTestUp/index.html'
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async goto() {
+        await this.page.goto(UploadPhotoPage.BASE_URL, { waitUntil: 'networkidle' })
+        await this.page.waitForSelector('#login-title', { state: 'visible' })
+        await expect(this.page.locator('#login-title')).toHaveText('Seja Bem-Vindo ao QA TestUp')
+    }
+
+    async login() {
+        await this.page.locator('#login-email').fill('teste@teste.com')
+        await this.page.locator('#login-password').fill('teste@123')
+        await this.page.locator('#btn-login-submit').click()
+        await expect(this.page.locator('#welcome-message')).toHaveText('Seja Bem-Vindo!')
+        await expect(this.page.locator('#welcome-logged-user')).toHaveText('QA Engineer')
+
+
+    }
+
+    async documentCenter() {
+        const tituloCentral = this.page.locator('.upload-section .upload-section-title');
+        await expect(tituloCentral).toHaveText('Central de Documentos do Usuário');
+
+        const fileUpload = this.page.locator('#upload-avatar');
+        await expect(fileUpload).toBeVisible();
+        await this.page.locator('#upload-avatar').setInputFiles('fixtures/avatar.png');
+
+        await expect(this.page.locator('#avatar-success-msg')).toHaveText('✓ Foto de perfil atualizada!')
+        await expect(this.page.locator('#welcome-avatar img')).toBeVisible();
+
+
+    }
+
+}
---
+++ tests/uploadPhoto.spec.ts
@@ -0,0 +1,17 @@
+import{test, expect} from '@playwright/test'
+import { UploadPhotoPage } from '../pages/uploadPhotoPage'      
+
+test.describe('Inserir foto de perfil', ()=>{
+
+    let uploadPhotoPage: UploadPhotoPage;
+
+    test.beforeEach(async({page})=>{
+        uploadPhotoPage = new UploadPhotoPage(page);
+        await uploadPhotoPage.goto()
+        await uploadPhotoPage.login()
+    })
+
+    test.only('Deve realizar upload com sucesso', async ({page})=>{
+        await uploadPhotoPage.documentCenter()
+    })
+})
